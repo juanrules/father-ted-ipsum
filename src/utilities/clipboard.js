902de963b97a5287544777e7c78785ac6a285074
Juan Pernia <juanpernia@gwsteam.com>
2020-02-18 19:43:28 +0000

    String utilities

---
+++ src/utilities/clipboard.js
@@ -0,0 +1,7 @@
+export const selectElement = element => {
+  const selection = window.getSelection();
+  const range = document.createRange();
+  range.selectNodeContents(element);
+  selection.removeAllRanges();
+  selection.addRange(range);
+};
