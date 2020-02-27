ec50ae908ed51f832bca597b4f834a3bf8353d41
Juan Pernia <juanpernia@gwsteam.com>
2020-02-27 20:30:17 +0000

    Moving the data to its own folder
    Also creating a file for handling the modal's content

---
+++ src/data/content.js
@@ -0,0 +1,11 @@
+export default {
+  modal: {
+    title: "About this project:",
+    paragraphs: [
+      "Hello there! You are interacting with a serveless ReactJs app hosted in an S3 bucket 💥 ",
+      "Behind the scenes there's an AWS pipeline that listens to new commits merged into the Master branch of this repo, which compiles the code and puts it into a public S3 bucket.",
+      "There are also unit tests to make sure everyhting is Honky dory everytime anything is changed in the code.",
+      "I've also made the illustrations. This project was done to play with all the above technologies."
+    ]
+  }
+};
