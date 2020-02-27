30b46e0c283c9086cb91a844f0d97dfaf5e7acc8
Juan Pernia <juanpernia@gwsteam.com>
2020-02-27 20:40:48 +0000

    Bringing modal's content from its data file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import "./App.scss";
-import data from "./data";
+import data from "./data/episodes";
 import { getEpisode } from "./services/dataHandler";
 import Canvas from "./components/canvas";
 import Toolbar from "./components/toolbar";
 import Sidebar from "./components/sidebar";
 import Modal from "./components/modal";
+import content from "./data/content";
+import { arrayToParagraphs } from "./utilities/strings";
 
 const App = () => {
   const [episode, setEpisode] = useState(getEpisode(data));
   const [hasBadWords, setHasBadWords] = useState(true);
-  const [hasLatinWords, setHasLatinWords] = useState(false);
+  const [hasLatinWords, setHasLatinWords] = useState(true);
   const [showCredits, setShowCredits] = useState(false);
 
   return (
@@ -29,32 +31,11 @@ const App = () => {
         />
         {showCredits && (
           <Modal toggleFn={() => setShowCredits(!showCredits)}>
-            <h3 className="modal__title">About this project:</h3>
-            <p>
-              Hello! Simply put, you are interacting with a serveless ReactJs
-              app hosted in an S3 bucket{" "}
-              <span role="img" aria-label="boom">
-                💥
-              </span>
-              .
-            </p>
-            <p>
-              Behind the scenes there's an AWS pipeline that listens to new
-              commits to the Master branch, compiles the code and puts it into a
-              public S3 bucket.
-            </p>
-            <p>
-              For the frontend, ReactJs is doing the trick with no backend
-              required
-            </p>
-            {/* <p>
-              There are also unit tests to make sure everyhting is Honky dory
-              everytime anything is changed in the code.
-            </p> */}
-            <p>
-              {/* I've also made the illustrations. This project was done to play
-              with all the above technologies. */}
-            </p>
+            {content.modal.title && (
+              <h3 className="modal__title">content.modal.title</h3>
+            )}
+            {content.modal.paragraphs &&
+              arrayToParagraphs(content.modal.paragraphs)}
           </Modal>
         )}
       </main>
