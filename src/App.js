b1887599f74abfcfeb1da679ffbfada33f061cb7
Juan Pernia <juanpernia@gwsteam.com>
2020-02-28 16:13:17 +0000

    Moving the copyAll() to the root app

--- src/App.js
+++ src/App.js
@@ -8,13 +8,23 @@ import Sidebar from "./components/sidebar";
 import Modal from "./components/modal";
 import content from "./data/content";
 import { arrayToParagraphs } from "./utilities/strings";
+import License from "./components/license";
+import MessageBar from "./components/messageBar";
+import { selectElement } from "./utilities/clipboard";
 
 const App = () => {
   const [episode, setEpisode] = useState(getEpisode(data));
   const [hasBadWords, setHasBadWords] = useState(true);
   const [hasLatinWords, setHasLatinWords] = useState(true);
   const [showCredits, setShowCredits] = useState(false);
+  const [showMessageBar, setShowMessageBar] = useState(false);
+  const copyAll = async () => {
+    selectElement(document.querySelector(".canvas"));
+    document.execCommand("copy");
+    setShowMessageBar(true);
 
+    setTimeout(() => setShowMessageBar(false), 2000);
+  };
   return (
     <>
       <Sidebar toggleFn={() => setShowCredits(!showCredits)} />
@@ -23,12 +33,16 @@ const App = () => {
           getNewEpisode={() => setEpisode(getEpisode(data))}
           filterBadWords={() => setHasBadWords(!hasBadWords)}
           filterLatinWords={() => setHasLatinWords(!hasLatinWords)}
+          copyAll={() => copyAll()}
         />
         <Canvas
           {...episode}
           hasBadWords={hasBadWords}
           hasLatinWords={hasLatinWords}
         />
+
+        <License />
+
         {showCredits && (
           <Modal toggleFn={() => setShowCredits(!showCredits)}>
             {content.modal.title && (
@@ -36,8 +50,18 @@ const App = () => {
             )}
             {content.modal.paragraphs &&
               arrayToParagraphs(content.modal.paragraphs)}
+            <p>
+              <br />
+              <a
+                className="button"
+                href="https://github.com/juanrules/father-ted-ipsum"
+              >
+                Checkout the Github repo
+              </a>
+            </p>
           </Modal>
         )}
+        <MessageBar show={showMessageBar} />
       </main>
     </>
   );
