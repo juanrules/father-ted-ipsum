bf8731eeda6ceba7c7068c4b15916a274cf5b63d
Juan Pernia <juanpernia@gwsteam.com>
2020-02-28 16:11:28 +0000

    another fix to the content

--- src/data/content.js
+++ src/data/content.js
@@ -3,7 +3,7 @@ export default {
     title: "About this project:",
     paragraphs: [
       "Hello there! You are interacting with a serveless ReactJs app hosted in an S3 bucket 💥 ",
-      "Behind the scenes there's an AWS pipeline that listens to new commits merged into the Master branch of this repo, which compiles the code and puts it into a public S3 bucket.",
+      "Behind the scenes there's an AWS pipeline that listens to new commits merged into the Master branch of a repo, which compiles the code and puts it into a public S3 bucket.",
       "There are also unit tests to make sure everyhting is Honky dory everytime anything is changed in the code.",
       "I've also made the illustrations. This project was done to play with all the above technologies."
     ]
