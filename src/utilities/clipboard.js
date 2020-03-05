a1073cfa8d0c5c12c314154d70ec54be49a6c419
Juan Pernia <juanpernia@gwsteam.com>
2020-03-05 20:02:31 +0000

    Moving the CopyAll() function to clipboard.js

--- src/utilities/clipboard.js
+++ src/utilities/clipboard.js
@@ -1,7 +1,15 @@
-export const selectElement = element => {
+const selectElement = element => {
   const selection = window.getSelection();
   const range = document.createRange();
   range.selectNodeContents(element);
   selection.removeAllRanges();
   selection.addRange(range);
+};
+
+export const copyAll = async (selector, callback) => {
+  selectElement(document.querySelector(selector));
+  document.execCommand("copy");
+  callback(true);
+
+  setTimeout(() => callback(false), 2000);
 };
